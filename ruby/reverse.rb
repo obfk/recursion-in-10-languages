cf800ca8ff2b3a88aaea356077091f0d491d5f4f
Devin Brown <devin.g.brown@gmail.com>
2014-05-10 07:42:14 -0600

    ruby reverse

--- ruby/reverse.rb
+++ ruby/reverse.rb
@@ -1,6 +1,6 @@
-def reverse(string, acc)
+def reverse(string, acc="")
   return acc if string.length == 0
   return reverse(string[1..-1], string[0] + acc)
 end
 
-puts reverse("hammer", "")
+puts reverse("hammer")
