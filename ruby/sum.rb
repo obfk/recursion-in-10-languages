d1239e0da0380ed08036abd0f9f8d52c098a0ce3
Devin Brown <devin.g.brown@gmail.com>
2014-05-10 07:42:31 -0600

    tail recursive solution for ruby/sum

--- ruby/sum.rb
+++ ruby/sum.rb
@@ -1,6 +1,18 @@
+# recursion
 def sum(array)
   return array[0] if array.size == 1
   return array[0] + sum(array[1..-1])
 end
 
-sum([1,2,3,4,5])
+puts sum([1,2,3,4,5])
+
+# tail recursion
+def tail_sum(array, acc=0)
+  if (array.size == 0)
+    return acc
+  else
+    return tail_sum(array[1..-1], array[0] + acc)
+  end
+end
+
+puts tail_sum([1,2,3,4,5])
