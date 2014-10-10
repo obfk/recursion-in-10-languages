0a40e140721ce76f74426f729e904a8514999656
Devin Brown <devin.g.brown@gmail.com>
2014-10-09 20:34:41 -0600

    THE MOST SEZY THING EVER #BALMERPEAK

---
+++ ruby/traverse.rb
@@ -0,0 +1,15 @@
+def dig(hash, flt = {})
+  hash.each { |k, v| v.is_a?(Hash) ? dig(v, flt) : flt[k] = v }
+  return flt
+end
+
+a = {a: {b: "cat", c: "dog"}, d: "pig"}
+b = {a: {b: "cat"}, d: "pig"}
+
+# dig(a)
+response = dig(a)
+valid    = dig(b)
+
+dig(a).each_pair do |k, v|
+  puts k if v == valid[k]
+end
